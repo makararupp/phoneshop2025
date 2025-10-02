6c879462737e0ad2c6e8b35dbcfdfcb8441b2658
makaraDevIt <makarasam68@gmail.com>
2025-10-02 18:35:31 +0700

    custom api definition

--- src/main/java/com/makara/phoneshop/PhoneShopApplication.java
+++ src/main/java/com/makara/phoneshop/PhoneShopApplication.java
@@ -1,5 +1,6 @@
 package com.makara.phoneshop;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
